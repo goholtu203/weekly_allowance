d6fa8870afe7bdf7106022687512dd3740cef116
goholtu203 <goholtushalom203@gmail.com>
2025-07-17 11:41:58 +0100

    first commit

---
+++ operators.py
@@ -0,0 +1,58 @@
+'''firstNumber = 3
+firstNumber+=3
+print (firstNumber)
+
+firstNumber*=5
+print(firstNumber)
+
+
+firstNumber/=3
+print(firstNumber)
+
+firstNumber**=3
+print (firstNumber)
+
+firstNumber//=3
+print(firstNumber)'''
+
+
+
+
+
+
+
+weekly_allowance = 2000
+weekly_allowance-=400
+print("I spend 400 naira on books and was left with ", weekly_allowance)
+weekly_allowance+=100
+print("I found 100 naira under my bed and added it to my allowance, making it", weekly_allowance)
+
+weekly_allowance-=250
+print("i bought snacks worth 250, and im now left with", weekly_allowance)
+weekly_allowance-=25/100*weekly_allowance
+
+print("i now gave 25% of my remaining balance to my siblings and left with", weekly_allowance)
+
+weekly_allowance-=1/3*weekly_allowance
+print("I now used one-third of whats left to buy data and left with", weekly_allowance)
+weekly_allowance//=2
+print("I split the remaining balance equally between savings and tithing and left with", weekly_allowance)
+
+weekly_allowance%=100
+
+print("my remainingn balace is ", weekly_allowance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
